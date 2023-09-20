4390bfab76dc4b3260fc18ca25a600aa8a5801ec
NOEL PAUL GEORGE <noelpaulgeorge@gmail.com>
2023-09-20 16:51:33 +0530

    Allowing all hosts

--- elevate_backend/settings.py
+++ elevate_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0+2c6-u07ta(4_bsj!4n2r33$g*5k&2x)iu)#mn3ew3kz!d3a+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
