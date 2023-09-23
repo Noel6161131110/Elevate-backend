e17a732ce2c3527a58df98ff17d37fabdeeb7e14
Noel6161131110 <noelpaulgeorge@gmail.com>
2023-09-23 14:14:34 +0530

    removed asgi

--- elevate_backend/settings.py
+++ elevate_backend/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'elevate_backend.wsgi.application'
 
-ASGI_APPLICATION = 'elevate_backend.asgi.application'
+#ASGI_APPLICATION = 'elevate_backend.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
