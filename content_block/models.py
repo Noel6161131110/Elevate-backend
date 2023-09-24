6ac287b30cbb7742e8129e8bb03378b7d62b828b
Noel6161131110 <noelpaulgeorge@gmail.com>
2023-09-24 20:22:07 +0530

    system changed , sign up added and content generatino soon to be added

--- content_block/models.py
+++ content_block/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=4)
+    email = models.TextField()
+    tags = models.TextField()
+    user_id = models.TextField(default='')
+    
+    
+class Content(models.Model):
+    user_id = models.TextField(default='')
+    content = models.TextField()
+    audio_location = models.TextField(default='')
+    
